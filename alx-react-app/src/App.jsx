78bbc43af69d3d7b9318b4e652c2c882244908ae
youssef douihich <youssefdouihich@gmail.com>
2025-02-19 13:22:58 +0100

    celcome

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './WelcomeMessage'
+import Welcome from './welcome'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Welcome />
     <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
