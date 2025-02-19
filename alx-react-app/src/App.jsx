375325e398cb4339247ae8457165f63e178d59a0
youssef douihich <youssefdouihich@gmail.com>
2025-02-19 14:05:35 +0100

    f m h

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header Component/Header'
-import MainContent from './components/Main Content Component/MainContent'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 
 
