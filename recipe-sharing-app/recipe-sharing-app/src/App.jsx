c469eb457108d9f85699ad4c13d0e7ddcadb2c81
youssef douihich <youssefdouihich@gmail.com>
2025-03-12 22:21:35 +0000

    zus

---
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -0,0 +1,18 @@
+
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+function App() {
+  
+
+  return (
+    <div>
+      <AddRecipeForm />
+      <RecipeList />
+
+    </div>
+    
+  )
+}
+
+export default App
