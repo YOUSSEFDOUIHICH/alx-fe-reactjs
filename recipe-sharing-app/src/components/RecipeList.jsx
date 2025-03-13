742e32e2607227da95d0d0fa31e7fcfd89aaad56
youssef douihich <youssefdouihich@gmail.com>
2025-03-13 21:33:08 +0000

    zustandd

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,17 +1,23 @@
-import { useRecipeStore } from './recipeStore';
+import useRecipeStore from '../store/recipeStore';
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
 
-    return (
-      <div>
-        {recipes.map(recipe => (
+  return (
+    <div>
+      <h2>Liste des Recettes</h2>
+      {recipes.length === 0 ? (
+        <p>Aucune recette pour le moment.</p>
+      ) : (
+        recipes.map((recipe) => (
           <div key={recipe.id}>
             <h3>{recipe.title}</h3>
             <p>{recipe.description}</p>
           </div>
-        ))}
-      </div>
-    );
-  };
-  export default RecipeList;
+        ))
+      )}
+    </div>
+  );
+};
+
+export default RecipeList;
