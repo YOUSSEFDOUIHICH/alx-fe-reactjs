eedf05f9953b2f3eee50e94f2f9b1d5754a3e39b
youssef douihich <youssefdouihich@gmail.com>
2025-03-16 18:35:02 +0000

    com

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,4 +1,4 @@
-import useRecipeStore from "./recipeStore";
+import useRecipeStore from "../store/recipeStore";
 
 const DeleteRecipeButton = ({ id }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
