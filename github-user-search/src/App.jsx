916f39fab6105076df701d634ea9b5245d51bde1
youssef douihich <youssefdouihich@gmail.com>
2025-04-08 12:10:56 +0100

    github

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-2xl font-bold">GitHub User Search</h1>
+      {/* Components will go here */}
+    </div>
+  )
+}
+
+export default App
