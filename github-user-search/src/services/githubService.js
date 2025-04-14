c872dcad808c72efa3364a25802bd8204a6349a4
youssef douihich <youssefdouihich@gmail.com>
2025-04-14 13:13:55 +0100

    tailwind

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const fetchAdvancedUserSearch = async (username, location, minRepos) => {
+const fetchAdvancedUserSearch = async (username, location, minRepos) => {
   let query = '';
   if (username) query += `${username} in:login`;
   if (location) query += ` location:${location}`;
@@ -9,3 +9,4 @@ export const fetchAdvancedUserSearch = async (username, location, minRepos) => {
   const response = await axios.get(`https://api.github.com/search/users?q=${encodeURIComponent(query)}`);
   return response.data.items;
 };
+export default fetchAdvancedUserSearch ;
